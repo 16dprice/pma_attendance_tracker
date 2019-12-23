631b07c639e50cd2e0c488dd85cce4f99a1c9556
DJ <donald.price077@topper.wku.edu>
2019-12-23 13:04:06 -0500

    Removing unnecessary nav items

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -6,24 +6,15 @@ export default class Navbar extends Component {
     render() {
         return(
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className="navbar-brand">&Phi;&Mu;&Alpha;</Link>
+                <Link to="/members" className="navbar-brand">&Phi;&Mu;&Alpha;</Link>
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav mr-auto">
-                        <li className="navbar-item">
-                            <Link to="/" className="nav-link">Home</Link>
-                        </li>
                         <li className="navbar-item">
                             <Link to="/members" className="nav-link">Members</Link>
                         </li>
                         <li className="navbar-item">
                             <Link to="/events" className="nav-link">Events</Link>
                         </li>
-                        <li className="navbar-item">
-                            <Link to="/attendance" className="nav-link">Attendance</Link>
-                        </li>
-                        {/*<li className="navbar-item">*/}
-                        {/*    <Link to="/events/4307577b-6143-4768-9ede-abfef64ce2c1" className="nav-link">Test</Link>*/}
-                        {/*</li>*/}
                     </ul>
                 </div>
             </nav>
