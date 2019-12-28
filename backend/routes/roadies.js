25043cd354a4ca08f2fc1b85f271c561516d8eb7
DJ <donald.price077@topper.wku.edu>
2019-12-27 23:24:06 -0600

    roadies route

---
+++ backend/routes/roadies.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { Roadies } = require('../sequelize');
+
+// read all
+router.route('/').get((req, res) => {
+    Roadies.findAll({
+        where: req.query
+    }).then(roadies => res.json(roadies));
+});
+
+// create
+router.route('/add').post((req, res) => {
+
+    const date = req.body.date;
+    const call_time = req.body.call_time;
+    const location = req.body.location;
+    const members_needed = req.body.members_needed;
+
+    Roadies.create({ date, call_time, location, members_needed })
+        .then(event => res.json(event))
+        .catch(err => res.json(err));
+
+});
