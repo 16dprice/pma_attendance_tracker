839948a59940c2b46fc3bddd0b90fdcdf9a47df3
DJ <donald.price077@topper.wku.edu>
2019-12-28 10:50:36 -0600

    roadies signup component (a little boilerplate with some comments)

---
+++ src/components/roadies-signup.component.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+// TODO: this component needs to update constantly while on the page
+// If people start signing up in meetings, then there could be a lot of traffic and I want it to live update so guys
+// don't try to sign up for something that just got taken.
+// Also, a lot of this needs to be wrapped in permission stuff, so I should probably work on a login system first.
+class RoadiesSignup extends Component {
+
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    // render() {
+    //
+    // }
+
+}
