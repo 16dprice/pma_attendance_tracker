60a9aa60ed94b99176b58f6103997f24df1fc475
DJ <djpric91@gmail.com>
2020-01-06 08:37:15 -0600

    adding roadie model to database

--- backend/sequelize.js
+++ backend/sequelize.js
@@ -5,6 +5,7 @@ const EventModel = require('./models/event.model');
 const MemberModel = require('./models/member.model');
 const AttendanceModel = require('./models/attendance.model');
 const RoadiesModel = require('./models/roadies.model');
+const RoadieSignUpModel = require('./models/roadie-signup.model');
 
 require('dotenv').config();
 
@@ -22,10 +23,14 @@ const Event = EventModel(connection, Sequelize);
 const Member = MemberModel(connection, Sequelize);
 const Attendance = AttendanceModel(connection, Sequelize);
 const Roadies = RoadiesModel(connection, Sequelize);
+const RoadieSignUp = RoadieSignUpModel(connection, Sequelize);
 
 
 Attendance.belongsTo(Event, { onDelete: 'cascade' }); // adds eventUuid reference to events table
 Attendance.belongsTo(Member); // adds memberMemberNumber reference to members table
+
+RoadieSignUp.belongsTo(Member, { onDelete: 'cascade' }); // adds memberMemberNumber reference to roadie sign up table
+RoadieSignUp.belongsTo(Roadies, { onDelete: 'cascade' }); // adds roadyUuid reference to roadie sign up table
 
 const resetDatabase = false;
 // const resetDatabase = true;
