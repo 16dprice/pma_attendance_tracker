dff7c678201f9ce8572edb35b261f77377e858af
DJ <donald.price077@topper.wku.edu>
2020-02-18 13:30:58 -0600

    seeing if taking away http will allow https site to retrieve data from api

--- src/components/members-list.component.js
+++ src/components/members-list.component.js
@@ -50,7 +50,7 @@ export default class MembersListComponent extends Component {
     }
 
     componentDidMount() {
-        axios.get('http://pmaiotamuattendance.neat-url.com:5000/api/members')
+        axios.get('www.pmaiotamuattendance.neat-url.com:5000/api/members')
             .then(res => {
                 this.setState({
                     members: res.data
